6b929a6a6b71089e9b884ea6f05acdb0d768924c
DS <derekstander@gmail.com>
2021-10-06 19:31:21 -0400

    modified:   public/api.js added console log for id. 	modified:   routes/api-routes.js api routes working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,49 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.get("/api/Workouts", (req, res) => {
+  Workout.find({})
+    .sort({ day:1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
-// get last workout, api/workouts
+router.put("/api/Workouts/:id", (req, res) => {
+  Workout.findOneAndUpdate(
+  {_id: req.params.id},
+  {$push: {exercises: req.body}
+  }).then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
-// put api/workouts + id
+router.post("/api/Workouts/", ({ body }, res) => {
+  Workout.create(body
+  ).then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
-// post api/workouts
-
-// api/workouts/range
-
+router.get("/api/workouts/range", (req,res) => {
+  Workout.find({})
+  .then(dbWorkout => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+  })
+      .catch(err => {
+          // If an error occurs, send the error to the client
+          res.json(err);
+      })
+})
 
 module.exports = router;
